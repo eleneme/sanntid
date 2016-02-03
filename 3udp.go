7511bde52ce4e500a305e7702da28d4da9804627
Kari Engen Guttulsrud <eparis@atlassian.com>
2016-02-03 15:45:03 +0100

    elene har rart hår i dag

--- 3udp.go
+++ 3udp.go
@@ -1,62 +1,93 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
 	"time"
 )
 
 const localAddr = "129.241.187.143"
 const broadcastAddr = "129.241.187.255"
-const receivePort = "30000"
-const sendPort = "20005"
+const receivePort = "20005"
+const sendPort = "30005"
 
-func errorCheck(err error){
-	if err!= nil{
+func errorCheck(err error) {
+	if err != nil {
 		fmt.Println("Error", err)
 	}
 }
 
-
-
 func main() {
-	serverAddr, err := net.ResolveUDPAddr ("udp", ":30000")
+	/*serverAddr, err := net.ResolveUDPAddr ("udp", ":30000")
+		errorCheck(err)
+
+		buffer := make([]byte,1024)
+
+		listenServer, err := net.ListenUDP("udp", serverAddr)
+		errorCheck(err)
+
+		defer listenServer.Close()
+
+	    	bytes, addr, err := listenServer.ReadFromUDP(buffer)
+	    	fmt.Println("Message recieved :", string(buffer[0:bytes]), "from address", addr)
+	    	errorCheck(err)
+
+	*/
+	serverAddrPort := "129.241.187.20:54321"
+
+	localAddr, err := net.ResolveUDPAddr("udp", serverAddrPort)
 	errorCheck(err)
- 
-	buffer := make([]byte,1024)
 
-    listenServer, err := net.ListenUDP("udp", serverAddr)
-    errorCheck(err)
+	connectServer, err := net.DialUDP("udp", nil, localAddr)
+	errorCheck(err)
 
-    defer listenServer.Close()
+	serverAddr, err := net.ResolveUDPAddr("udp", ":20005")
+	errorCheck(err)
 
-    	bytes, addr, err := listenServer.ReadFromUDP(buffer)
-    	fmt.Println("Message recieved :", string(buffer[0:bytes]), "from address", addr)
-    	errorCheck(err)
+	listenServer, err := net.ListenUDP("udp", serverAddr)
+	errorCheck(err)
 
-    serverAddrPort := "129.241.187.23:20005"
+	defer listenServer.Close()
 
-    localAddr, err := net.ResolveUDPAddr("udp", serverAddrPort)
-    errorCheck(err)
+	defer connectServer.Close()
 
+	message := "Hello can you hear ne"
 
-    connectServer, err := net.DialUDP("udp", nil, localAddr)
-    errorCheck(err)
+	_, err = connectServer.Write([]byte(message + "\x00"))
+	errorCheck(err)
+	buffer := make([]byte, 1024)
+	//fmt.Println("Melding sendt")
 
-   defer connectServer.Close()
+	input := bufio.NewReader(os.Stdin)
 
-
-   message := "Hello its me"
-   for{
-	   _, err = connectServer.Write([]byte(message + "\x00"))
-	   errorCheck(err)
-
-	   //buffer := make([]byte,1024)
-
-	   numBytes, _ , _ := connectServer.ReadFromUDP(buffer)
-	   fmt.Println(buffer[:numBytes])
-	   time.Sleep(1)
+	func UDPListen(conn *net.UDPConn, chanListen <-chan config.NetworkMessage) {
+	for {
+		message := make([]byte, 1024)
+		length, recieveAddress, _ := conn.ReadFromUDP(message)
+		recievedMessage := config.NetworkMessage{recieveAddress: recieveAddress.IP.String(), data: message, length: length}
+		chanListen <- recievedMessage
 	}
+}
 
+func UDPSend() {
+	_msgSend, _ := input.ReadString('\n')
+
+	_, err = connectServer.Write([]byte(msgSend + "\x00"))
+}
+
+	for {
+
+		numBytes, _, _ := listenServer.ReadFromUDP(buffer)
+		fmt.Println(string(buffer[:numBytes]))
+		//fmt.Println("Melding mottatt")
+		msgSend, _ := input.ReadString('\n')
+
+		_, err = connectServer.Write([]byte(msgSend + "\x00"))
+		//errorCheck(err)
+
+		time.Sleep(1)
+	}
 
 }
