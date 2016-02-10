3cec8b8fd706dab03a9327a3d1a83bb744265f72
aarskog <eparis@atlassian.com>
2016-02-10 13:39:50 +0100

    Driver

--- 3udp.go
+++ 3udp.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"time"
 )
 
 const localAddr = "129.241.187.143"
@@ -17,6 +16,31 @@ func errorCheck(err error) {
 	if err != nil {
 		fmt.Println("Error", err)
 	}
+}
+
+func UDPListen() {
+
+	serverAddr, err := net.ResolveUDPAddr("udp", ":20005")
+	errorCheck(err)
+
+	listenServer, err := net.ListenUDP("udp", serverAddr)
+	errorCheck(err)
+	buffer := make([]byte, 1024)
+	numBytes, _, _ := listenServer.ReadFromUDP(buffer)
+	fmt.Println(string(buffer[:numBytes]))
+
+}
+func UDPSend() {
+	serverAddrPort := "129.241.187.20:54321"
+	localAddr, err := net.ResolveUDPAddr("udp", serverAddrPort)
+	errorCheck(err)
+
+	connectServer, err := net.DialUDP("udp", nil, localAddr)
+	errorCheck(err)
+	input := bufio.NewReader(os.Stdin)
+	msgSend, _ := input.ReadString('\n')
+
+	_, err = connectServer.Write([]byte(msgSend + "\x00"))
 }
 
 func main() {
@@ -35,7 +59,7 @@ func main() {
 	    	errorCheck(err)
 
 	*/
-	serverAddrPort := "129.241.187.20:54321"
+	/*serverAddrPort := "129.241.187.20:54321"
 
 	localAddr, err := net.ResolveUDPAddr("udp", serverAddrPort)
 	errorCheck(err)
@@ -57,37 +81,12 @@ func main() {
 
 	_, err = connectServer.Write([]byte(message + "\x00"))
 	errorCheck(err)
-	buffer := make([]byte, 1024)
-	//fmt.Println("Melding sendt")
-
-	input := bufio.NewReader(os.Stdin)
-
-	func UDPListen(conn *net.UDPConn, chanListen <-chan config.NetworkMessage) {
-	for {
-		message := make([]byte, 1024)
-		length, recieveAddress, _ := conn.ReadFromUDP(message)
-		recievedMessage := config.NetworkMessage{recieveAddress: recieveAddress.IP.String(), data: message, length: length}
-		chanListen <- recievedMessage
-	}
-}
-
-func UDPSend() {
-	_msgSend, _ := input.ReadString('\n')
-
-	_, err = connectServer.Write([]byte(msgSend + "\x00"))
-}
+	*/
 
 	for {
 
-		numBytes, _, _ := listenServer.ReadFromUDP(buffer)
-		fmt.Println(string(buffer[:numBytes]))
-		//fmt.Println("Melding mottatt")
-		msgSend, _ := input.ReadString('\n')
-
-		_, err = connectServer.Write([]byte(msgSend + "\x00"))
-		//errorCheck(err)
-
-		time.Sleep(1)
+		go UDPSend()
+		go UDPListen()
 	}
 
 }
